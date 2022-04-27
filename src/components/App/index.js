4045b9f1ed5213c049b9bbfb10af9a334aaf14e1
Dioselyn Colina <dioze09@gmail.com>
2022-04-27 08:51:46 -0500

    Add footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { TasksProvider } from "../../context/Tasks";
 import { Container } from '../Container';
+import { Footer } from '../Footer';
 import './App.css';
 
 
 
 function App() {
   return (
+    <React.Fragment>
     <TasksProvider>
       <Container/>
     </TasksProvider>
+    <Footer/>
+    </React.Fragment>
       
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/Footer.css';
+
+
+function Footer() {
+    return (
+        <footer>
+            <p className='Footer color-primary'>Made by <a className="color-gradient" href='https://twitter.com/developdctech' target="_blank" rel='noreferrer'>Dioselyn Colina</a></p>
+        </footer>
+    )
+}
+
+export { Footer };
