1fe944a23811c90fae016d3ca305d2bd27c04ea8
Dioselyn Colina <dioze09@gmail.com>
2022-05-03 13:53:55 -0500

    Delete ContextTasks

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { NewTask } from "../NewTask";
+import { Tasks } from "../Tasks";
+import { Modal } from '../Modal'
+import { TasksContext } from "../../context/Tasks";
+import { CreateTaskButtonMobile } from "../NewTask/CreateTaskButtonMobile";
 import { TasksProvider } from "../../context/Tasks";
 import { Container } from '../Container';
 import { Footer } from '../Footer';
--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,52 +1,29 @@
 import React from "react";
-import { NewTask } from "../NewTask";
-import { Tasks } from "../Tasks";
-import { Modal } from '../Modal'
-import { TasksContext } from "../../context/Tasks";
-import { CreateTaskButtonMobile } from "../NewTask/CreateTaskButtonMobile";
-
-
 import './css/Container.css';
 
 
 
 
-function Container() {
+function Container({openModal, setOpenModal}) {
     return (
-        <TasksContext.Consumer>
-        {({
-         openModal,
-         setOpenModal,
-        }) => (
-        <React.Fragment>
         <div className="Container">
             <NewTask/>
             <Tasks/>
-
         </div>
 
-      
-            
             {!!openModal && ( //IF IS TRUE
                 <Modal/>
             )}
 
         
         <CreateTaskButtonMobile
-            setOpenModal={ setOpenModal}
+            setOpenModal={ setOpenModal }
         />
 
-        </React.Fragment>
-      
-       
-         )
-        }
-       
-      
-
-        </TasksContext.Consumer>
-    )
-
+    );
 }
+       
+
+
 
 export { Container };
